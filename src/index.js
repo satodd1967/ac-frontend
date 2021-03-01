7babb8ee76e146e33be1aee169c8e4871ba3e454
satodd1967 <seth.todd@outlook.com>
2021-02-28 21:33:53 -0600

    moved the store out of index.js and into its own store file.

--- src/index.js
+++ src/index.js
@@ -3,18 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux'
-import thunk from 'redux-thunk';
-import { Provider } from 'react-redux'
-
-const users = () => []
-const reducer = combineReducers({
-  users
-})
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+import { Provider } from 'react-redux';
+import store from './store';
 
 ReactDOM.render(
   <Provider store={store}>
