011346fd99ff177267676056dd7b4d32cc4535ae
satodd1967 <seth.todd@outlook.com>
2021-03-05 14:43:14 -0600

    Added a MainBody component with a LeftNav component rendered in it and a LeftNav Component that is currently blank.  Deleted the withRouter out of the App.js file for now.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home';
 import SignupForm from './components/SignupForm'
 import { connect } from 'react-redux';
 import { getCurrentUser } from './actions/currentUser';
-import { Route, withRouteer } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 
 class App extends React.Component {
 
