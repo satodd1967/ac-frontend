15ec051254015d6623d1d119d7a7db77c63d89a6
satodd1967 <seth.todd@outlook.com>
2021-03-11 21:43:43 -0600

    Created a Trip component.

---
+++ src/components/Challenge.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Challenge = (props) => {
+
+    return (
+        <div className="challenge-show">
+            
+        </div>
+    )
+}
+
+export default Challenge
