48cb5ec51d94e81f49679f2665cb0c217525f620
satodd1967 <seth.todd@outlook.com>
2021-03-14 19:45:55 -0500

    Created a ChallengeRanking card for the ChallengeRank Container.

---
+++ src/components/ChallengeRankingCard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ChallengeCards = (props) => {
+
+    return (
+        <div className="challenge-cards">
+            <li><h4><Link to={`/challenges/${props.challenge.id}`}>{props.challenge.attributes.name}</Link></h4></li>
+            <li>{props.challenge.attributes.description}</li>
+            <br/>
+        </div>
+    )
+}
+
+export default ChallengeCards
