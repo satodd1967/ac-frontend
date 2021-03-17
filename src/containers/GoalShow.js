7a25454909ef3a3ad454d0b28cc9d980359fe23b
satodd1967 <seth.todd@outlook.com>
2021-03-17 18:57:33 -0500

    Updated the GoalShow.js file with the beginnings of a show container.

--- src/containers/GoalShow.js
+++ src/containers/GoalShow.js
@@ -1,54 +1,20 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import ChallengeCard from '../components/ChallengeCard';
-import ChallengeOwner from '../components/ChallengeOwners';
-import ChallengeRanking from './ChallengeRanking';
-import LogCards from '../components/LogCards'
 import { Link } from 'react-router-dom';
 
-const ChallengeShow = (props) => {
-
-    const challenge = props.challenges.find(challenge => {
-        return challenge.id === props.challengeId
-    })
-
-    const challengeOwner = challenge ? <ChallengeOwner challenge={challenge}/> : <p>No Challenge</p>
-
-    const challengeCard = challenge ? <ChallengeCard challenge={challenge}/> : <p>No Challenge</p>
-
-    const challengeRanking = challenge ? <ChallengeRanking challenge={challenge}/> : <p>No Challenge</p>
-
-    const logs = challenge ? challenge.attributes.logs.map(log => {
-        return <li key={log.id}><LogCards key={log.id} log={log}/></li>
-        }) : <p>No Challenge</p>
+const GoalShow = (props) => {
 
     return (
         <div className="challenge-show">
-            <div className="challenge-show-header">
-                <h1>{challenge ? challenge.attributes.name : `No Challenge`}</h1>
-                <h3>{challengeOwner}</h3>
-                <h3>Ranking</h3>
-                <h3>{challengeRanking}</h3>
-            </div>
-            <div className="challenge-show-details">
-                <h1>Details</h1>
-                <ul>
-                    {challengeCard}
-                </ul>
-            </div>
-            <div className="challenge-show-diary-logs">
-                <h1>Diary Logs</h1>
-                {logs}
-            </div>
+            
         </div>
     )
 }
 
 const mapStateToProps = state => {
     return {
-        user: state.currentUser,
-        challenges: state.challenges
+        
     }
 }
 
-export default connect(mapStateToProps)(ChallengeShow)
+export default connect(mapStateToProps)(GoalShow)
