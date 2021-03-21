2ccc1f7d2aae0602a88e152fe159639ee9e96ff4
satodd1967 <seth.todd@outlook.com>
2021-03-21 18:14:55 -0500

    Updated the logs action file to include an update action and a delete action.

--- src/actions/logs.js
+++ src/actions/logs.js
@@ -81,3 +81,65 @@ export const sendLog = (logData, history, user) => {
   
     }
   }
+
+  export const updateLog = (logData, history, user, logId) => {
+    return dispatch => {
+      const updateLogData = {
+        log_date: logData.logDate,
+        worked_out: logData.workedOut,
+        tracked_food: logData.trackedFood,
+        weight: logData.weight,
+        body_fat: logData.bodyFat,
+        active_calories: logData.activeCalories,
+        calories: logData.calories,
+        user_id: user.id,
+      }
+      return fetch(`http://localhost:3001/api/logs/${logId}`, {
+        credentials: "include",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(updateLogData)
+      })
+        .then(resp => resp.json())
+        .then(response => {
+          if (response.error) {
+            alert(response.error)
+          } else {
+            console.log("New Log Post", response)
+            dispatch(setLogs())
+            dispatch(setChallenges())
+            dispatch(getCurrentUser(history))
+          }
+        })
+        .catch(console.log)
+  
+    }
+  }
+
+  export const deleteLog = (logId, history) => {
+    return dispatch => {
+      return fetch(`http://localhost:3001/api/logs/${logId}`, {
+        credentials: "include",
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+        .then(resp => resp.json())
+        .then(response => {
+          if (response.error) {
+            alert(response.error)
+          } else {
+            dispatch(setLogs())
+            dispatch(setChallenges())
+            dispatch(getCurrentUser(history))
+            history.push('/')
+          }
+        })
+        .catch(console.log)
+  
+    }
+  
+  }
