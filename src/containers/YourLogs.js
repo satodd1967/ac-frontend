b69a66fea85e754b1b4042190ea6861653c315a5
satodd1967 <seth.todd@outlook.com>
2021-03-21 15:57:23 -0500

    Needed to change the userId prop for logs to currentUserId to account for the check in logCards.

--- src/containers/YourLogs.js
+++ src/containers/YourLogs.js
@@ -5,7 +5,7 @@ import { connect } from 'react-redux';
 const YourLogs = (props) => {
 
     const logs = props.logs.map(log => {
-        return <li key={log.id}><LogCards key={log.id} log={log} userId={props.user.id}/></li>
+        return <li key={log.id}><LogCards key={log.id} log={log} currentUserId={props.user.id}/></li>
         })
     
     return (
