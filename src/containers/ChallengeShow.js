e38c4f08540626d4a9be38ce298344dcf7ccef9b
satodd1967 <seth.todd@outlook.com>
2021-03-21 14:15:44 -0500

    Updated challenge show to have both a edit and a view goals button if the challenge belongs to the user.

--- src/containers/ChallengeShow.js
+++ src/containers/ChallengeShow.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import ChallengeCard from '../components/ChallengeCard';
 import ChallengeOwner from '../components/ChallengeOwners';
+import ChallengeEditButton from '../components/ChallengeEditButton';
+import GoalsViewButton from '../components/GoalsViewButton';
 import ChallengeRanking from './ChallengeRanking';
 import LogCards from '../components/LogCards'
 
@@ -10,6 +12,16 @@ const ChallengeShow = (props) => {
     const challenge = props.challenges.find(challenge => {
         return challenge.id === props.challengeId
     })
+
+    const currentUserChallengeGoal = challenge.attributes.challenge_goals.find(challengeGoal => {
+        return challengeGoal.user_id === props.user.id
+    }) 
+
+    const editButton = challenge && challenge.attributes.user_id === props.user.id ? 
+        <ChallengeEditButton challengeId={challenge.id}/> : ""
+    
+    const viewGoals = challenge && challenge.attributes.user_id === props.user.id ?
+        <GoalsViewButton challengeId={challenge.id} currentUserCGId={currentUserChallengeGoal.id}/> : ""
 
     const challengeOwner = challenge ? <ChallengeOwner challenge={challenge}/> : <p>No Challenge</p>
 
@@ -26,6 +38,8 @@ const ChallengeShow = (props) => {
             <div className="challenge-show-header">
                 <h1>{challenge ? challenge.attributes.name : `No Challenge`}</h1>
                 <h3>{challengeOwner}</h3>
+                {editButton}
+                {viewGoals}
                 <h3>Ranking</h3>
                 <h3>{challengeRanking}</h3>
             </div>
