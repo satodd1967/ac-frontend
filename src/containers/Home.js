ec0b407465a353e5072d14f06f72a43d308aa718
satodd1967 <seth.todd@outlook.com>
2021-03-25 21:34:29 -0500

    Deleted some unecessary console.logs out of the home routes.

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -48,10 +48,6 @@ class Home extends React.Component {
                         <Route exact path='/goals/:id' component={GoalShow}/>
                         <Route exact path='/challenges/:id/challenge_goals/new' component={ChallengeGoalCreateForm}/>
                         <Route exact path='/challenge_goals/:id/edit' render={props => {
-                            console.log("Params", props.match.params.id)
-                            console.log("ChallengeGoal", this.props.user.challenge_goals.find(challengeGoal => {
-                                return challengeGoal.id === props.location.challengeGoalId
-                            }))
                             const challengeGoal = this.props.user.challenge_goals.find(challengeGoal => challengeGoal.id === props.location.challengeGoalId)
                             return <ChallengeGoalEditForm challengeGoal={challengeGoal} history={props.history}/>
                             }
