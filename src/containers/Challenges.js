9bbf481a6e297ab21547f6c25463de0f93529423
satodd1967 <seth.todd@outlook.com>
2021-03-26 14:16:40 -0500

    Added a join challenge setup to the Challenges container that shows a link for join challenge if you are not already in a challenge.  This link takes you to create new challenge goals for that challenge.

--- src/containers/Challenges.js
+++ src/containers/Challenges.js
@@ -3,19 +3,22 @@ import ChallengeCards from '../components/ChallengeCards';
 import ChallengeOwners from '../components/ChallengeOwners';
 import ChallengeRanking from './ChallengeRanking';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Challenges = (props) => {
 
     const challenges = props.challenges.map(challenge => {
         const currentUserChallengeGoal = challenge.attributes.challenge_goals.find(challengeGoal => {
             return challengeGoal.user_id === props.user.id
-        }) 
+        })
+        const joinChallenge = !currentUserChallengeGoal ? <li><Link to={`/challenges/${challenge.id}/challenge_goals/new`}>Join This Challenge!</Link></li> : ""
         return <ul key={challenge.id}>
             <ChallengeCards challenge={challenge} currentUserCGId={currentUserChallengeGoal ? currentUserChallengeGoal.id : "null"}/>
             <li key={challenge.attributes.user.id}><ChallengeOwners key={challenge.attributes.user.id} challenge={challenge}/></li>
             <br/>
             <h4>Ranking</h4>
             <ChallengeRanking challenge={challenge} type={"full"}/>
+            {joinChallenge}
             </ul>
         })
     
