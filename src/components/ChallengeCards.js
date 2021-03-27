de08fca8de329972f5bc8935af5675cc140ffc0f
satodd1967 <seth.todd@outlook.com>
2021-03-27 15:44:16 -0500

    Destructured the props in ChallengeCards.

--- src/components/ChallengeCards.js
+++ src/components/ChallengeCards.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ChallengeCards = (props) => {
+const ChallengeCards = ({ challenge }) => {
 
     return (
         <div className="challenge-cards">
-            <li><h4><Link to={`/challenges/${props.challenge.id}`}>{props.challenge.attributes.name}</Link></h4></li>
-            <li>{props.challenge.attributes.description}</li>
+            <li><h4><Link to={`/challenges/${challenge.id}`}>{challenge.attributes.name}</Link></h4></li>
+            <li>{challenge.attributes.description}</li>
             <br/>
         </div>
     )
